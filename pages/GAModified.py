4f16fb69136125665911afa77717565bb4039701
Muhammad Fahmi <96282872+fahmihilmi@users.noreply.github.com>
2024-12-12 21:11:07 +0800

    Update GAModified.py

--- pages/GAModified.py
+++ pages/GAModified.py
@@ -17,14 +17,14 @@ def read_csv_to_dict(file_path):
     return program_ratings
 
 # Path to the CSV file
-file_path = '/content/program_ratings.csv'
+file_path = 'pages/program_ratings.csv'
 
 # Get the data in the required format
 program_ratings_dict = read_csv_to_dict(file_path)
 
 # Print the result (you can also return or process it further)
 for program, ratings in program_ratings_dict.items():
-    print(f"'{program}': {ratings},")
+    st.write(f"'{program}': {ratings},")
 
 
 import random
@@ -150,9 +150,10 @@ genetic_schedule = genetic_algorithm(initial_best_schedule, generations=GEN, pop
 
 final_schedule = initial_best_schedule + genetic_schedule[:rem_t_slots]
 
-print("\nFinal Optimal Schedule:")
+st.write("\nFinal Optimal Schedule:")
 for time_slot, program in enumerate(final_schedule):
-    print(f"Time Slot {all_time_slots[time_slot]:02d}:00 - Program {program}")
+    st.write(f"Time Slot {all_time_slots[time_slot]:02d}:00 - Program {program}")
 
-print("Total Ratings:", fitness_function(final_schedule))
+st.write("Total Ratings:", fitness_function(final_schedule))
+
 
